9a1efa3e450b84209fc2fc3ad01a0f59ba3d1147
user <user@admin>
2017-01-04 23:00:11 +0900

    문제 5번 풀었음

--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -3,15 +3,20 @@ package practice01;
 public class Prob5 {
 
 	public static void main(String[] args) {
-		for(int i=0; i<10; i++){
-			for(int j=0; j<10; j++){
-				
-				if(j==3||j==6||j==9){
-					System.out.print((i)+""+(j)+"짝");
-					if(i==3||i==6||i==9){
-						System.out.print("짝");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (i == 0) {
+					if (j == 3 || j == 6 || j == 9) {
+						System.out.println((j) + "짝");
 					}
-					System.out.println("");
+				} else {
+					if (j == 3 || j == 6 || j == 9) {
+						System.out.print((i) + "" + (j) + "짝");
+						if (i == 3 || i == 6 || i == 9) {
+							System.out.print("짝");
+						}
+						System.out.println("");
+					}
 				}
 			}
 		}
